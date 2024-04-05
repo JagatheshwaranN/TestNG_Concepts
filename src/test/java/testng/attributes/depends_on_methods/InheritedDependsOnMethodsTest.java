3d53aa5c29373e357ede8fc04c62ec055d8ddba8
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-04-05 13:42:18 +0530

    TestNG - Notes Add In-progress. Timeout and GroupDependency using XML scenarios added

---
+++ src/test/java/testng/attributes/depends_on_methods/InheritedDependsOnMethodsTest.java
@@ -0,0 +1,61 @@
+package testng.attributes.depends_on_methods;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * "DependsOnMethods" - DependsOnMethods is used to depend on the test case
+ * before the dependent test case run. All the methods you depend on must have
+ * run and succeeded for you to run.
+ * <p>
+ * In TestNG, the dependsOnMethods attribute is used to specify dependencies
+ * between test methods. When a test method is annotated with dependsOnMethods,
+ * it indicates that the test method should only be executed if the specified
+ * methods, referred to as its dependencies, have successfully passed.
+ * <p>
+ * The primary use of the dependsOnMethods attribute is to control the order
+ * of test method execution and ensure that certain methods are only executed
+ * when their dependencies succeed. This can be helpful in scenarios where
+ * specific actions need to be performed in a sequence or when one test method
+ * sets up a precondition for another.
+ * <p>
+ * The code exemplifies the synergy between TestNG annotations and inheritance to
+ * construct a coherent test suite. In the superclass, a method facilitates logging
+ * in, while the subclass inherits this functionality along with additional test
+ * methods for browsing products and logging out. TestNG's dependsOnMethods attribute
+ * ensures precise sequencing of test execution, adhering to defined dependencies.
+ * This arrangement showcases the power of inheritance in TestNG, allowing for the
+ * reuse of shared test methods across various test cases while preserving their
+ * interdependencies.
+ * 
+ * @author Jagatheshwaran N
+ */
+
+public class InheritedDependsOnMethodsTest extends Superclass {
+
+	// Test method for browsing products, depends on 'doLogin'
+	@Test(dependsOnMethods = "doLogin")
+	public void browseProducts() {
+		// Print a message indicating the action being performed
+		System.out.println("Browse and search for products on the online shopping platform");
+	}
+
+	// Test method for logout, depends on 'browseProducts'
+	@Test(dependsOnMethods = { "browseProducts" })
+	public void doLogout() {
+		// Print a message indicating the action being performed
+		System.out.println("Logout from the online shopping platform");
+	}
+
+}
+
+class Superclass {
+
+	// Test method for login
+	@Test
+	public void doLogin() {
+		// Print a message indicating the action being performed
+		System.out.println("Login to the online shopping platform");
+	}
+
+}
---
+++ src/test/java/testng/attributes/others/TimeoutTest.java
@@ -0,0 +1,32 @@
+package testng.attributes.others;
+
+import org.testng.annotations.Test;
+
+/**
+ * In TestNG, "timeout" is a parameter that defines the maximum time that a method can take
+ * for execution. The timeout parameter is handy if the execution time is a constraint during
+ * testing, or the tester wants to take precautions against the endless execution of tests.
+ * <p>
+ * The code snippet demonstrates how to set a timeout for a test method using TestNG's @Test
+ * annotation. In this example, the doLogin() method is expected to complete within 1 second
+ * (1000 milliseconds). If the method exceeds this time limit, it will be considered failed.
+ * This feature is useful for ensuring that test methods do not hang indefinitely, allowing
+ * the test execution framework to proceed even if a test method takes longer than expected.
+ */
+public class TimeoutTest {
+
+    // Test method for login with priority 1 and a timeout of 1000 milliseconds
+    @Test(priority = 1, timeOut = 1000)
+    public void doLogin() {
+        try {
+            // Simulate some processing time for login (750 milliseconds)
+            Thread.sleep(750);
+        } catch (InterruptedException e) {
+            // Throw a runtime exception if the thread sleep is interrupted
+            throw new RuntimeException(e);
+        }
+        // Print a message indicating the action being performed
+        System.out.println("Login to the online shopping platform");
+    }
+
+}
